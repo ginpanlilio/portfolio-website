78ae03eed4e554df065b7884791cfc20716e8e78
ginpanlilio <116160771+ginpanlilio@users.noreply.github.com>
2023-10-11 15:37:35 +0800

    Add jQuery functions for hamburger list

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
 console.log("Welcome to my website. Learn more about what I do.");
+
+var name = "Gin";
+var age = 41;
+var hobbies = "jogging, rowing, yoga, journaling, wine and coffee chats";
+var infoAbout = {
+    name: name,
+    age: age,
+    hobbies: hobbies,
+}
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+      }
+    }
